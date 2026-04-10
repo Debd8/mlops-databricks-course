a013c2fac3ae9bfad8e581e2a2a1349cf983905d
Debarati Dutta <debaratidutta@Debaratis-MacBook-Pro.local>
2026-04-09 21:21:51 -0400

    Add whl path

--- notebooks/lecture2.marvel_data_preprocessing.py
+++ notebooks/lecture2.marvel_data_preprocessing.py
@@ -1,12 +1,29 @@
 # Databricks notebook source
+import os
+import subprocess
 
-# MAGIC %pip install -e ..
-# MAGIC %restart_python
+# Set up Databricks or local MLflow tracking
+def is_databricks():
+    return "DATABRICKS_RUNTIME_VERSION" in os.environ
 
 # COMMAND ----------
-# from pathlib import Path
-# import sys
-# sys.path.append(str(Path.cwd().parent / 'src'))
+# Run uv build in terminal to build the wheel file for marvel_characters package before running this notebook
+# Databricks only, comment out when running locally
+databricks_user = "your-email@gmail.com"
+whl_path = f"/Workspace/Users/{databricks_user}/.bundle/dev/marvel-characters/files/dist/marvel_characters-0.1.0-py3-none-any.whl"
+
+result = subprocess.run(
+    ["pip", "install", whl_path],
+    capture_output=True, text=True
+)
+print(result.stdout)
+print(result.stderr)
+
+# COMMAND ----------
+if is_databricks():
+    base_path = f"/Workspace/Users/{databricks_user}/.bundle/dev/marvel-characters/files"
+else:
+    base_path = os.path.abspath("../")
 
 # COMMAND ----------
 import pandas as pd
@@ -17,8 +34,7 @@ from pyspark.sql import SparkSession
 from marvel_characters.config import ProjectConfig
 from marvel_characters.data_processor import DataProcessor
 
-config = ProjectConfig.from_yaml(config_path="../project_config_marvel.yml", env="dev")
-
+config = ProjectConfig.from_yaml(config_path=f"{base_path}/project_config_marvel.yml", env="dev")
 logger.info("Configuration loaded:")
 logger.info(yaml.dump(config, default_flow_style=False))
 
@@ -27,7 +43,7 @@ logger.info(yaml.dump(config, default_flow_style=False))
 # Load the Marvel characters dataset
 spark = SparkSession.builder.getOrCreate()
 
-filepath = "../data/marvel_characters_dataset.csv"
+filepath = f"{base_path}/data/marvel_characters_dataset.csv"
 
 # Load the data
 df = pd.read_csv(filepath)
